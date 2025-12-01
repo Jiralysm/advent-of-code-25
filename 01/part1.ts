11dca09a0760fbb6b0c56b3d6c3a085f5412ab99
kie.ac <automation@kie.ac>
2025-12-01 13:42:07 +0000

    aoc day 1

--- 01/part1.ts
+++ 01/part1.ts
@@ -1,15 +1,11 @@
-// Advent of Code 2025 - Day 1, Part 1: Secret Entrance
+// Advent of Code 2025, Day 01, Part 1: Name
 // https://adventofcode.com/2025/day/1
 
-import { writeAnswer, readInput } from "../helpers/functions"
-const s = { p: 50, h: 0 }
+import { readInput, writeAnswer } from "../functions"
+const s = {}
 
 for (const line of await readInput(import.meta.url)) {
-  const d = +line.slice(1)
-  s.p = line[0] === "R"
-    ? (s.p + d) % 100
-    : (s.p - d + 10000) % 100
-  if (s.p === 0) s.h++
+
 }
 
-writeAnswer(s.h, import.meta.url)
+writeAnswer(0, import.meta.url)
--- 01/part2.ts
+++ 01/part2.ts
@@ -1,20 +1,11 @@
-// Advent of Code 2025 - Day 1, Part 2: Secret Entrance
+// Advent of Code 2025, Day 01, Part 2: Name
 // https://adventofcode.com/2025/day/1
 
-import { writeAnswer, readInput } from "../helpers/functions"
-const s = { p: 50, h: 0 }
+import { readInput, writeAnswer } from "../functions"
+const s = {}
 
 for (const line of await readInput(import.meta.url)) {
-  const d = +line.slice(1)
-  const r = line[0] === "R"
 
-  let f = r ? (100 - s.p) % 100 : s.p % 100
-  if (f === 0) f = 100
-  if (f <= d) s.h += 1 + ((d - f) / 100 | 0)
-
-  s.p = r
-    ? (s.p + d) % 100
-    : (s.p - d + 10000) % 100
 }
 
-writeAnswer(s.h, import.meta.url)
+writeAnswer(0, import.meta.url)
