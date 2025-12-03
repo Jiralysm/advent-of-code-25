4f4a96218f22695b567b206beea94354b1fb4205
Kieran <commit@kie.ac>
2025-12-03 13:22:59 +0000

    fix: Update Day 3 Part 1 and Part 2 logic for sum calculation and adjust comments

--- 03/part1.ts
+++ 03/part1.ts
@@ -1,11 +1,24 @@
-// Advent of Code 2025, Day 03, Part 1: Name
+// Advent of Code 2025, Day 3, Part 1: Lobby
 // https://adventofcode.com/2025/day/3
 
 import { readInput, writeAnswer } from "../helpers/functions"
-const s = {}
+
+let sum = 0
 
 for (const line of await readInput(import.meta.url)) {
+  let best = 0
+  let bestRight = -1
 
+  for (let i = line.length - 1; i >= 0; i--) {
+    const d = line.charCodeAt(i) - 48
+    if (bestRight >= 0) {
+      const val = d * 10 + bestRight
+      if (val > best) best = val
+    }
+    if (d > bestRight) bestRight = d
+  }
+
+  sum += best
 }
 
-writeAnswer(0, import.meta.url)
+writeAnswer(sum, import.meta.url)
--- 03/part2.ts
+++ 03/part2.ts
@@ -1,11 +1,36 @@
-// Advent of Code 2025, Day 03, Part 2: Name
+// Advent of Code 2025, Day 3, Part 2: Lobby
 // https://adventofcode.com/2025/day/3
 
 import { readInput, writeAnswer } from "../helpers/functions"
-const s = {}
+
+let sum = 0
 
 for (const line of await readInput(import.meta.url)) {
+  let need = 12
+  let i = 0
+  let out = ""
 
+  while (need > 0) {
+    const remain = line.length - i
+    const windowEnd = line.length - (need - 1)
+    let bestDigit = -1
+    let bestIndex = i
+
+    for (let j = i; j < windowEnd; j++) {
+      const d = line.charCodeAt(j) - 48
+      if (d > bestDigit) {
+        bestDigit = d
+        bestIndex = j
+        if (d === 9) break
+      }
+    }
+
+    out += String(bestDigit)
+    i = bestIndex + 1
+    need--
+  }
+
+  sum += Number(out)
 }
 
-writeAnswer(0, import.meta.url)
+writeAnswer(sum, import.meta.url)
