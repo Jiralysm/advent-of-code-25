dec52f2f3f9337828f5d5e2bb0047e5de577ce0d
Kieran <commit@kie.ac>
2025-12-06 16:20:58 +0000

    complete day 6 (pt 1+2)

--- 06/part1.ts
+++ 06/part1.ts
@@ -1,11 +1,74 @@
-// Advent of Code 2025, Day 06, Part 1: Name
+// Advent of Code 2025, Day 06, Part 1: Trash Compactor
 // https://adventofcode.com/2025/day/6
 
-import { readInput, writeAnswer } from "../helpers/functions"
-const s = {}
+import { readInput, writeAnswer } from "../helpers/functions";
+const g = await readInput(import.meta.url);
 
-for (const line of await readInput(import.meta.url)) {
+const h = g.length;
+const w = g[0].length;
 
+let total = 0;
+let x = 0;
+
+while (x < w) {
+  let empty = true;
+  for (let y = 0; y < h; y++) {
+    if (g[y][x] !== " ") {
+      empty = false;
+      break;
+    }
+  }
+
+  if (empty) {
+    x++;
+    continue;
+  }
+
+  const cols: number[] = [];
+
+  while (x < w) {
+    let blank = true;
+    for (let y = 0; y < h; y++) {
+      if (g[y][x] !== " ") {
+        blank = false;
+        break;
+      }
+    }
+    if (blank) break;
+
+    cols.push(x);
+    x++;
+  }
+
+  const nums: number[] = [];
+  let op: string | null = null;
+  const bottom = h - 1;
+
+  for (const cx of cols) {
+    const c = g[bottom][cx];
+    if (c === "+" || c === "*") {
+      op = c;
+      break;
+    }
+  }
+
+  for (let y = 0; y < bottom; y++) {
+    let s = "";
+    for (const cx of cols) {
+      const c = g[y][cx];
+      if (c >= "0" && c <= "9") s += c;
+    }
+    if (s !== "") nums.push(Number(s));
+  }
+
+  let res = op === "+" ? 0 : 1;
+
+  for (const n of nums) {
+    if (op === "+") res += n;
+    else res *= n;
+  }
+
+  total += res;
 }
 
-writeAnswer(0, import.meta.url)
+writeAnswer(total, import.meta.url);
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-![TypeScript](https://shields.io/badge/TypeScript-3178C6?logo=TypeScript&logoColor=FFF&style=flat-square) ![Shield](https://img.shields.io/badge/2025-%E2%AD%90%20%2010-gray?logo=adventofcode&labelColor=8a2be2)
+![TypeScript](https://shields.io/badge/TypeScript-3178C6?logo=TypeScript&logoColor=FFF&style=flat-square) ![Shield](https://img.shields.io/badge/2025-%E2%AD%90%20%2012-gray?logo=adventofcode&labelColor=8a2be2)
 # Advent of Code 2025 🎄
 
 Welcome to my AoC 2025 solutions. I’m using this repository to follow my journey, try out ideas and keep my strategies coherent throughout the month. Every day has a folder, input file and solutions in TypeScript. I use Bun for quick iteration and low friction tools. If you are solving the puzzles yourself, you’ll have an opportunity to compare notes or snag anything here that may help!
@@ -34,3 +34,4 @@ I also keep a blog displaying my thoughts, views and opinions on each day's prob
 | [Day 03: Lobby](https://adventofcode.com/2025/day/3) | [2025/03](https://github.com/Jiralysm/advent-of-code-25/tree/main/03) | ⭐⭐ |
 | [Day 04: Printing Department](https://adventofcode.com/2025/day/4) | [2025/04](https://github.com/Jiralysm/advent-of-code-25/tree/main/04) | ⭐⭐ |
 | [Day 05: Cafeteria](https://adventofcode.com/2025/day/5) | [2025/05](https://github.com/Jiralysm/advent-of-code-25/tree/main/05) | ⭐⭐ |
+| [Day 06: Trash Compactor](https://adventofcode.com/2025/day/6) | [2025/06](https://github.com/Jiralysm/advent-of-code-25/tree/main/06) | ⭐⭐ |
