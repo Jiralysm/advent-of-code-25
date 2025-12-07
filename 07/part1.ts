cfb7e11f159e8a37192240f87069d9baed2ed3e8
Kieran <commit@kie.ac>
2025-12-07 13:07:33 +0000

    complete day 7 (pt 1+2)

--- 07/part1.ts
+++ 07/part1.ts
@@ -1,11 +1,50 @@
-// Advent of Code 2025, Day 07, Part 1: Name
+// Advent of Code 2025, Day 07, Part 1: Laboratories
 // https://adventofcode.com/2025/day/7
 
-import { readInput, writeAnswer } from "../helpers/functions"
-const s = {}
+import { readInput, writeAnswer } from "../helpers/functions";
 
-for (const line of await readInput(import.meta.url)) {
+const g = await readInput(import.meta.url);
+const h = g.length;
+const w = g[0].length;
 
+let sx = 0;
+let sy = 0;
+
+for (let y = 0; y < h; y++) {
+  for (let x = 0; x < w; x++) {
+    if (g[y][x] === "S") {
+      sx = x;
+      sy = y;
+    }
+  }
 }
 
-writeAnswer(0, import.meta.url)
+let splits = 0;
+const stack: { x: number; y: number }[] = [{ x: sx, y: sy + 1 }];
+const seen = new Set<string>();
+
+while (stack.length) {
+  const { x, y } = stack.pop()!;
+  const key = x + "," + y;
+
+  if (seen.has(key)) continue;
+  seen.add(key);
+
+  if (x < 0 || x >= w || y < 0 || y >= h) continue;
+
+  const c = g[y][x];
+
+  if (c === "^") {
+    splits++;
+
+    stack.push({ x: x - 1, y: y + 1 });
+    stack.push({ x: x + 1, y: y + 1 });
+    continue;
+  }
+
+  if (c === ".") {
+    stack.push({ x, y: y + 1 });
+  }
+}
+
+writeAnswer(splits, import.meta.url);
--- 07/part2.ts
+++ 07/part2.ts
@@ -1,11 +1,66 @@
-// Advent of Code 2025, Day 07, Part 2: Name
+// Advent of Code 2025, Day 07, Part 2: Laboratories
 // https://adventofcode.com/2025/day/7
 
-import { readInput, writeAnswer } from "../helpers/functions"
-const s = {}
+import { readInput, writeAnswer } from "../helpers/functions";
 
-for (const line of await readInput(import.meta.url)) {
+const g = await readInput(import.meta.url);
+const h = g.length;
+const w = g[0].length;
 
+let sx = 0;
+let sy = 0;
+
+for (let y = 0; y < h; y++) {
+  for (let x = 0; x < w; x++) {
+    if (g[y][x] === "S") {
+      sx = x;
+      sy = y;
+    }
+  }
 }
 
-writeAnswer(0, import.meta.url)
+const dp: number[][] = Array.from({ length: h }, () =>
+  Array(w).fill(0)
+);
+
+if (sy + 1 < h) dp[sy + 1][sx] = 1;
+
+let total = 0;
+
+for (let y = sy + 1; y < h; y++) {
+  for (let x = 0; x < w; x++) {
+    const c = dp[y][x];
+    if (!c) continue;
+
+    if (y === h - 1) {
+      total += c;
+      continue;
+    }
+
+    const below = g[y + 1][x];
+
+    if (below === ".") {
+      dp[y + 1][x] += c;
+    } else if (below === "^") {
+      const ny = y + 1;
+      const lx = x - 1;
+      const rx = x + 1;
+
+      if (lx >= 0) {
+        dp[ny][lx] += c;
+      } else {
+        total += c;
+      }
+
+      if (rx < w) {
+        dp[ny][rx] += c;
+      } else {
+        total += c;
+      }
+    } else {
+      dp[y + 1][x] += c;
+    }
+  }
+}
+
+writeAnswer(total, import.meta.url);
