55c04375d7e3b3390fbf81ff55df5563bb31e0fc
kie.ac <automation@kie.ac>
2025-12-09 05:48:08 +0000

    automation: create day 9

---
+++ 09/part1.ts
@@ -0,0 +1,11 @@
+// Advent of Code 2025, Day 09, Part 1: Name
+// https://adventofcode.com/2025/day/9
+
+import { readInput, writeAnswer } from "../helpers/functions"
+const s = {}
+
+for (const line of await readInput(import.meta.url)) {
+
+}
+
+writeAnswer(0, import.meta.url)
