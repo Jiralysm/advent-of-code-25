b3981d57676a68561b61e46ccb3c3047d4a11b38
Kieran <commit@kie.ac>
2025-12-11 16:42:57 +0000

    complete day 11 (pt 1+2)

--- 11/part1.ts
+++ 11/part1.ts
@@ -1,11 +1,26 @@
-// Advent of Code 2025, Day 11, Part 1: Name
+// Advent of Code 2025, Day 11, Part 1: Reactor
 // https://adventofcode.com/2025/day/11
 
-import { readInput, writeAnswer } from "../helpers/functions"
-const s = {}
+import { readInput, writeAnswer } from "../helpers/functions";
 
-for (const line of await readInput(import.meta.url)) {
+const lines = await readInput(import.meta.url);
+const g = new Map<string, string[]>();
 
+for (const line of lines) {
+  const [left, right] = line.split(": ");
+  g.set(left, right.split(" "));
 }
 
-writeAnswer(0, import.meta.url)
+const memo = new Map<string, number>();
+
+function dfs(u: string): number {
+  if (memo.has(u)) return memo.get(u)!;
+  if (u === "out") return 1;
+  const next = g.get(u) || [];
+  let sum = 0;
+  for (const v of next) sum += dfs(v);
+  memo.set(u, sum);
+  return sum;
+}
+
+writeAnswer(dfs("you"), import.meta.url);
--- 11/part2.ts
+++ 11/part2.ts
@@ -1,11 +1,35 @@
-// Advent of Code 2025, Day 11, Part 2: Name
+// Advent of Code 2025, Day 11, Part 2: Reactor
 // https://adventofcode.com/2025/day/11
 
-import { readInput, writeAnswer } from "../helpers/functions"
-const s = {}
+import { readInput, writeAnswer } from "../helpers/functions";
 
-for (const line of await readInput(import.meta.url)) {
+const lines = await readInput(import.meta.url);
+const g = new Map<string, string[]>();
 
+for (const line of lines) {
+  const [left, right] = line.split(": ");
+  g.set(left, right.split(" "));
 }
 
-writeAnswer(0, import.meta.url)
+const memo = new Map<string, number>();
+
+function key(u: string, d: number, f: number) {
+  return u + "|" + d + "|" + f;
+}
+
+function dfs(u: string, haveD: number, haveF: number): number {
+  if (u === "out") return haveD & haveF;
+  const k = key(u, haveD, haveF);
+  if (memo.has(k)) return memo.get(k)!;
+
+  let nd = haveD | (u === "dac" ? 1 : 0);
+  let nf = haveF | (u === "fft" ? 1 : 0);
+
+  let sum = 0;
+  for (const v of g.get(u) || []) sum += dfs(v, nd, nf);
+
+  memo.set(k, sum);
+  return sum;
+}
+
+writeAnswer(dfs("svr", 0, 0), import.meta.url);
